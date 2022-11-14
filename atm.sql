c2b29f5caeec7af41c8e3e7dcb4478fe0e5bc2e2
MANJUNATH KOTABAL <manjunathmkotabal99@gmail.com>
2022-11-14 18:04:31 +0530

    commit by ghouse

--- atm.sql
+++ atm.sql
@@ -6,6 +6,3 @@ create table admin(admin_id varchar(20) primary key,password varchar(20) unique,
 create table transaction(transaction_id int NOT NULL AUTO_INCREMENT primary key,t_user_id int,user_name varchar(25),withdrawn_or_deposited bool,amount int,trans_pin int,foreign key(t_user_id) references user(user_id) on delete set null on update cascade);
 create table atm(atmamount int default 0,two_k int default 0,half_k int default 0,hundreds int default 0,two_hundreds int default 0);
 insert into atm values(0,0,0,0,0);
-create table transaction(transaction_id int NOT NULL AUTO_INCREMENT primary key,t_user_id int,user_name varchar(25),withdrawn_or_deposited bool,amount int,trans_pin int,foreign key(t_user_id) references user(user_id) on delete set null on update cascade);
-create table atm(atmamount int default 0,two_k int default 0,half_k int default 0,hundreds int default 0,two_hundreds int default 0);
-insert into atm values(0,0,0,0,0);
